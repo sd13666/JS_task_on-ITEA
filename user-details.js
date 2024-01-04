b7fc72859e443115ab94d947588ce8454745fadd
sd13666 <sssddd1366628@gmail.com>
2024-01-04 19:45:50 +0200

    deleted coments

--- user-details.js
+++ user-details.js
@@ -1,18 +1,6 @@
-// fetch(`https://jsonplaceholder.typicode.com/users/1`)
-//   .then((response) => response.json())
-//   .then((user) => {
-//     document.getElementById("user-name").textContent = user.name;
-//     document.getElementById("user-email").textContent = user.email;
-//     document.getElementById("user-phone").textContent = user.phone;
-//     // Додайте інші поля для відображення повної інформації про користувача
-//     document.getElementById("user-address").textContent = `${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}`;
-//     document.getElementById("user-website").textContent = user.website;
-//   });
-
-
 fetch('https://jsonplaceholder.typicode.com/users')
   .then((response) => response.json())
-  .then((users) => {
+  .then((users) => {console.log(users)
     users.forEach((user) => {
       const userInfo = document.createElement('div');
       userInfo.innerHTML = `
